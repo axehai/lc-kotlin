574e5e2f134415b89cf14ff836e5ac1c84cdb2ef
axehai <shukla.akshaykumar@gmail.com>
2026-03-21 14:39:50 +0530

    solve:LC 424 longest repeating character replacement

---
+++ src/main/kotlin/lc/sliding_window/lc424/problems/Solution.kt
@@ -0,0 +1,23 @@
+package com.axehai.lc.sliding_window.lc424.problems
+
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        val freq = IntArray(26)
+        var left = 0
+        var maxFreq = 0
+        var ans = 0
+        for (right in s.indices) {
+            val index = s[right] - 'A'
+            freq[index]++
+            maxFreq = maxOf(maxFreq, freq[index])
+
+            while ((right - left) + 1 - maxFreq > k) {
+                freq[s[left]-'A']--
+                left++
+            }
+
+            ans=maxOf((right-left+1),ans)
+        }
+        return ans
+    }
+}
---
+++ src/test/kotlin/lc/problems/lc424/SolutionTest.kt
@@ -0,0 +1,34 @@
+package lc.problems.lc424
+
+import com.axehai.lc.sliding_window.lc424.problems.Solution
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class SolutionTest {
+
+    @Test
+    fun example1() {
+        val actual = Solution().characterReplacement("ABAB", 2)
+        assertEquals(4, actual)
+    }
+
+    @Test
+    fun example2() {
+        val actual = Solution().characterReplacement("AABABBA", 1)
+        assertEquals(4, actual)
+    }
+
+    @Test
+    fun edge_singleChar() {
+        val actual = Solution().characterReplacement("A", 0)
+        assertEquals(1, actual)
+    }
+
+    @Test
+    fun edge_noReplacement() {
+        val actual = Solution().characterReplacement("ABCDE", 0)
+        assertEquals(1, actual)
+    }
+
+}
